871ebed4253c28438ad003c5282b22aabbdd64c6
Pedro Costa <pcesar_costa@hotmail.com>
2020-09-30 11:11:37 -0300

    Update app.py
    add werkzeug.exceptions

--- collecting-and-structuring-data for-a-book-recommendation-system/api/app.py
+++ collecting-and-structuring-data for-a-book-recommendation-system/api/app.py
@@ -1,7 +1,10 @@
 import os
+import json
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
+from flask import request, Response
+from werkzeug.exceptions import HTTPException
 
 from resources import Books
 from resources import BooksStars
@@ -15,6 +18,11 @@ rest = Api(app)
 rest.add_resource(GenerateToken, '/token')
 rest.add_resource(Books, '/books')
 rest.add_resource(BooksStars, '/bookstars')
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = json.dumps({"status": 404, "error": "Not found", "description": "The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again."})
+    return Response(response, mimetype="application/json", status=400)
 
 if __name__ == "__main__":
     app.run(debug=True)
