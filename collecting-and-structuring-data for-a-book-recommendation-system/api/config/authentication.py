8251805239b6845aee190bb91aba4752c811a29b
Pedro Costa <pcesar_costa@hotmail.com>
2020-09-30 11:00:27 -0300

    Create authentication.py

---
+++ collecting-and-structuring-data for-a-book-recommendation-system/api/config/authentication.py
@@ -0,0 +1,36 @@
+import os
+import jwt
+import json
+from flask import Response, request
+from flask_httpauth import HTTPTokenAuth
+#from functools import wraps
+
+auth = HTTPTokenAuth(scheme='Bearer')
+
+'''
+# TOKEN FORMAT
+{
+    "iss": domain,
+    "exp": expiration
+}
+'''
+
+@auth.verify_token
+def verify_token(jwt_token):
+    #print(request.headers)
+    try:
+        secret = os.getenv("JWT_SECRET")
+        jwt_decoded = jwt.decode(jwt_token, secret)
+        return jwt_decoded
+    except:
+        return False
+    return False
+
+@auth.error_handler
+def auth_error(status):
+    response = json.dumps({
+            "error": "Unauthorized",
+            "description": 'Authorization not informed, expired or invalid.',
+            "status_code": status
+        })
+    return Response(response, mimetype="application/json", status=status)
